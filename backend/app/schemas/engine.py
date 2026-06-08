a0e850a165b5424497ead95d0f3829c8b66c7a1d
Priyanshu <pksingh69313@gmail.com>
2026-06-08 23:11:43 +0530

    add request and response api

--- backend/app/schemas/engine.py
+++
@@ -1,1 +0,0 @@
-#request/response shapes (CodeSubmitRequest)
---
+++ backend/app/schemas/user.py
@@ -0,0 +1,19 @@
+#request/response shapes (CodeSubmitRequest)
+
+from pydantic import BaseModel, EmailStr
+from uuid import UUID
+from datetime import datetime
+
+#jo data frontend se aaega
+class UserSyncRequest(BaseModel):
+    email: EmailStr
+    auth_provider_id: str
+
+#jo data backend wapas frontend ko bhejega
+class UserResponse(BaseModel):
+    id: UUID
+    email: EmailStr
+    created_at: datetime
+
+    class config:
+        from_attritbutes = True #sqlalchemy ORM objects ko directly JSON mein convert karne ke liye
