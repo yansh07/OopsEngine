94164d0c6a5a01c62aaecf2c604923032de92a1b
Priyanshu <pksingh69313@gmail.com>
2026-06-08 23:48:59 +0530

    add auth logic

--- backend/app/api/auth.py
+++ backend/app/api/auth.py
@@ -1,1 +1,37 @@
 # endpoints to handle/verify clerk/github tokens
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
+
+from app.db.database import AsyncSessionLocal
+from app.db.models import Users
+from app.schemas.user import UserSyncRequest, UserResponse
+
+router = APIRouter()
+
+#dependency = har api request ke liye naya db session banana and close karna
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
+
+@router.post("/sync", response_model=UserResponse)
+async def sync_user(user_in: UserSyncRequest, db: AsyncSession = Depends(get_db)):
+    #check if user already exist based on clerk id
+    query = select(Users).where(Users.auth_provider_id == user_in.auth_provider_id)
+    result = await db.execute(query)
+    existing_user = result.scalars().first()
+
+    #If exist, return their data
+    if existing_user:
+        return existing_user
+    
+    #if new user, insert into postgres
+    new_user = Users(
+        email=user_in.email,
+        auth_provider_id=user_in.auth_provider_id
+    )
+    db.add(new_user)
+    await db.commit()
+    await db.refresh(new_user)
+
+    return new_user
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,1 +1,21 @@
 #the entry point - initializes fastapi and includes routers
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app.api import auth
+
+app = FastAPI(title="OopsEngine API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+#attach auth router
+app.include_router(auth.router, prefix="/api/auth", tags=["Authenticated"])
+
+@app.get("/")
+async def root():
+    return {"status": "OopsEngine Backend is Running!!"}
