ad115b5e025dd42f92ccee2edd654339682fba47
Priyanshu <pksingh69313@gmail.com>
2026-06-10 14:49:18 +0530

    reconfigured user sync api

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,7 +7,7 @@ app = FastAPI(title="OopsEngine API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://oopsengine.vercel.app/"],
+    allow_origins=["https://oopsengine.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
