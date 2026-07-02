41469b278c79c05325e35b8cc51ab1201a4e5bf8
Priyanshu <pksingh69313@gmail.com>
2026-07-03 00:57:55 +0530

    add locust test run

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -8,7 +8,14 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "postgresql+asyncpg://postgres:postgres@db:5432/oopsengine"
 
-engine = create_async_engine(DATABASE_URL, echo=False)
+engine = create_async_engine(
+    DATABASE_URL, 
+    echo=False,
+    pool_size=20, #20 concurrent user handle karne ke liye
+    max_overflow=20, #peak load par extra 20 connection allow karne ke liye
+    pool_timeout=10, #agar line lagna pade toh max 10s wait karega, 30s nahi
+    pool_recycle=1800 #30min mein stable connection reset karega
+)
 
 AsyncSessionLocal = async_sessionmaker(
     bind=engine,
--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api import auth, execute
 
+
 app = FastAPI(title="OopsEngine API")
 
 app.add_middleware(
@@ -20,3 +21,28 @@ app.include_router(execute.router, prefix="/api/execute", tags=["Execution Engin
 @app.get("/")
 async def root():
     return {"status": "OopsEngine Backend is Running!!"}
+
+
+#dummy api for p95----------------------------------------------------------
+
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+# Apne folders ke naam ke hisab se niche wale imports sahi kar lena:
+from .db.database import AsyncSessionLocal  # Aapki db file se sessionmaker
+from .db import Users  # Aapka User table model jiski file se bhi aata ho
+
+# Ek async database session generator dependency
+async def get_async_db():
+    async with AsyncSessionLocal() as session:
+        yield session
+
+@app.get("/test-load-db")
+async def test_load_db(db: AsyncSession = Depends(get_async_db)):
+    try:
+        from sqlalchemy import select
+        # Async way me Postgres se top 10 users fetch karna
+        result = await db.execute(select(Users).limit(10))
+        users = result.scalars().all()
+        return {"status": "success", "count": len(users)}
+    except Exception as e:
+        return {"status": "error", "message": str(e)}
